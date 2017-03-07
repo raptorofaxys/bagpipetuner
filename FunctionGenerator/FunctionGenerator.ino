72497daadd74c008f742543d1906ef9497280db4
fhamel <fhamel@gmail.com>
2017-03-07 09:00:06 -0500

    Submit function generator code.

---
+++ FunctionGenerator/FunctionGenerator.ino
@@ -0,0 +1,163 @@
+#define DEFAULT_PRINT (&Serial)
+
+void Ln(Print* p = DEFAULT_PRINT)
+{
+	p->println("");
+}
+
+void ClearScreen(Print* p = DEFAULT_PRINT)
+{
+	p->print("\x1B[2J");
+}
+
+void MoveCursor(int x, int y, Print* p = DEFAULT_PRINT)
+{
+	p->print("\x1B[");
+	p->print(y + 1);
+	p->print(";");
+	p->print(x + 1);
+	p->print("H");
+}
+
+void Space(Print* p = DEFAULT_PRINT)
+{
+	p->print(" ");
+}
+
+void Cls(Print* p = DEFAULT_PRINT)
+{
+	p->print(char(27));
+	p->print("[2J");
+}
+
+void PrintFloat(float f, int decimals = 10, Print* p = DEFAULT_PRINT)
+{
+	if (f < 0)
+	{
+		p->print("-");
+		f = -f;
+	}
+	else
+	{
+		p->print(" ");
+	}
+
+	int b = int(f);
+	p->print(b);
+	p->print(".");
+	f -= b;
+	for (int i = 0; i < decimals; ++i)
+	{
+		f *= 10.0f;
+		int a = int(f);
+		p->print(a);
+		f -= a;
+	}
+}
+
+void PrintHex(int h, Print* p = DEFAULT_PRINT)
+{
+	static char const* hex = "0123456789ABCDEF";
+	p->print(hex[(h & 0xF000) >> 12]);
+	p->print(hex[(h & 0x0F00) >> 8]);
+	p->print(hex[(h & 0x00F0) >> 4]);
+	p->print(hex[(h & 0x000F) >> 0]);
+}
+
+void PrintStringInt(char const* s, int v, Print* p = DEFAULT_PRINT)
+{
+	p->print(s);
+	p->print(": ");
+	p->print(v);
+}
+
+void PrintStringLong(char const* s, long v, Print* p = DEFAULT_PRINT)
+{
+	p->print(s);
+	p->print(": ");
+	p->print(v);
+}
+
+void PrintStringFloat(char const* s, float f, int decimals = 5, Print* p = DEFAULT_PRINT)
+{
+	p->print(s);
+	p->print(": ");
+	PrintFloat(f, decimals);
+}
+
+void setup()
+{
+	Serial.begin(115200);
+}
+
+class FunctionGenerator
+{
+public:
+	FunctionGenerator(int inputPin, int outputPin)
+		: m_analogReadDelay(outputPin) // round-robin the analogwrites
+		, m_inputPin(inputPin)
+		, m_outputPin(outputPin)
+		, m_lastUs(0)
+		, m_toggleDeltaUs(0)
+		, m_toggleUsAccumulator(0)
+		, m_currentValue(false)
+	{
+		pinMode(inputPin, INPUT);
+		pinMode(outputPin, OUTPUT);
+	}
+
+	void Update()
+	{
+		unsigned long nowUs = micros();
+		unsigned long deltaUs = nowUs - m_lastUs;
+		m_lastUs = nowUs;
+
+		m_toggleUsAccumulator += deltaUs;
+
+		--m_analogReadDelay;
+		if (m_analogReadDelay <= 0)
+		{
+			m_analogReadDelay = 10;
+			int inValue = analogRead(m_inputPin);
+			int frequency = map(inValue, 0, 1023, 40, 1000);
+			//int frequency = 100;
+
+			//PrintStringInt("Frequency", frequency); Ln();
+			//int frequency = 400;
+
+			m_toggleDeltaUs = 500000UL / frequency;
+		}
+
+		if (m_toggleUsAccumulator > m_toggleDeltaUs)
+		{
+			m_toggleUsAccumulator -= m_toggleDeltaUs;
+			m_currentValue = !m_currentValue;
+			digitalWrite(m_outputPin, m_currentValue);
+		}
+	}
+
+private:
+	int m_analogReadDelay;
+	int m_inputPin;
+	int m_outputPin;
+	unsigned long m_lastUs;
+	unsigned long m_toggleDeltaUs;
+	unsigned long m_toggleUsAccumulator;
+	bool m_currentValue;
+};
+
+void loop()
+{
+	FunctionGenerator f1(A0, 2);
+	FunctionGenerator f2(A1, 3);
+	FunctionGenerator f3(A2, 4);
+	FunctionGenerator f4(A3, 5);
+	for (;;)
+	{
+		//Serial.println("---");
+		f1.Update();
+		f2.Update();
+		f3.Update();
+		f4.Update();
+	}
+}
