f1efd290eaba547cb5438500795346f71e6e95ff
fhamel <fhamel@gmail.com>
2018-04-15 13:02:25 -0400

    Move temperament-related code out of the TunerChannel and into the tuning code file

--- Tuner/tuner_55_tuning.ino
+++ Tuner/tuner_55_tuning.ino
@@ -96,3 +96,158 @@ float g_noteSemitoneRatio[] =
 #else
 #error Unknown temperament!
 #endif
+
+#if (TEMPERAMENT == EQUAL_TEMPERAMENT)
+// Converts a fundamental frequency in Hz to a MIDI note index.  Slow.
+int GetMidiNoteIndexForFrequency(float frequency, float a440)
+{
+    if (frequency < 0.0f)
+    {
+        return -1;
+    }
+
+    // Shift the note down half a semitone so the frequency interval that maps to a MIDI note is centered on the note.
+    frequency *= QUARTERTONE_DOWN;
+
+    int note = A440_NOTE;
+    float a440_2 = 2.0f * a440;
+    while (frequency < a440)
+    {
+        frequency *= 2.0f;
+        note -= 12;
+    }
+    while (frequency > a440_2)
+    {
+        frequency *= 0.5f;
+        note += 12;
+    }
+    while (frequency > a440)
+    {
+        frequency *= SEMITONE_DOWN;
+        note += 1;
+    }
+    return note;
+}
+
+// Compute the fundamental frequency of a given MIDI note index.  Slow.
+float GetFrequencyForMidiNoteIndex(int note, float a440)
+{
+    if (note < 0.0f)
+    {
+        return -1.0f;
+    }
+
+    float result = a440;
+
+    while (note < A440_NOTE)
+    {
+        note += 12;
+        result *= 0.5f;
+    }
+
+    while (note > A880_NOTE)
+    {
+        note -= 12;
+        result *= 2.0f;
+    }
+
+    while (note > A440_NOTE)
+    {
+        --note;
+        result *= SEMITONE_UP;
+    }
+
+    return result;
+}
+#elif (TEMPERAMENT == JUST_TEMPERAMENT)
+// Converts a fundamental frequency in Hz to a MIDI note index.  Slow.
+int GetMidiNoteIndexForFrequency(float frequency, float a440)
+{
+    DEBUG_PRINT_STATEMENTS(Serial.write("GetMidiNoteIndexForFrequency()"); Ln(););
+
+    if (frequency <= 0.0f)
+    {
+        DEBUG_PRINT_STATEMENTS(Serial.write("earlying out, freq is <= 0"); Ln(););
+        return -1;
+    }
+
+    // Shift the note down up a semitone so the frequency interval that maps to a MIDI note is centered on the note.
+    // (Algorithm here is different from the one for equal temperament, we have to shift in the other direction.)
+    frequency *= QUARTERTONE_UP;
+
+    DEBUG_PRINT_STATEMENTS(PrintStringFloat("Frequency", frequency); Ln(););
+
+    int note = A440_NOTE;
+    float a440_2 = 2.0f * a440;
+    DEBUG_PRINT_STATEMENTS(PrintStringFloat("a440", a440); Ln(););
+    while (frequency < a440)
+    {
+        DEBUG_PRINT_STATEMENTS(Serial.write("Double up"); Ln(););
+        frequency *= 2.0f;
+        note -= 12;
+    }
+    while (frequency >= a440_2)
+    {
+        DEBUG_PRINT_STATEMENTS(Serial.write("Double down"); Ln(););
+        frequency *= 0.5f;
+        note += 12;
+    }
+
+    DEBUG_PRINT_STATEMENTS(PrintStringFloat("Centered frequency", frequency); Ln(););
+
+    auto rangeIndex = 0;
+    float rangeHigh = g_noteSemitoneRatio[rangeIndex] * a440;
+    float rangeLow = -1.0f;
+    for (;;)
+    {
+        rangeLow = rangeHigh;
+        ++rangeIndex;
+        rangeHigh = g_noteSemitoneRatio[rangeIndex] * a440;
+        DEBUG_PRINT_STATEMENTS(
+        {
+            PrintStringFloat("rangeLow", rangeLow); Ln();
+        PrintStringFloat("rangeHigh", rangeHigh); Ln();
+        });
+        if ((frequency >= rangeLow) && (frequency < rangeHigh))
+        {
+            DEBUG_PRINT_STATEMENTS(PrintStringFloat("found note!", rangeHigh); Ln(););
+            break;
+        }
+        ++note;
+        DEBUG_PRINT_STATEMENTS(PrintStringInt("note", note); Ln(););
+    }
+    DEBUG_PRINT_STATEMENTS(PrintStringInt("Final note", note); Ln(););
+    return note;
+}
+
+// Compute the fundamental frequency of a given MIDI note index.  Slow.
+float GetFrequencyForMidiNoteIndex(int note, float a440)
+{
+    DEBUG_PRINT_STATEMENTS(Serial.write("GetFrequencyForMidiNoteIndex()"); Ln(); );
+
+    if (note < 0.0f)
+    {
+        return -1.0f;
+    }
+
+    float result = a440;
+
+    while (note < A440_NOTE)
+    {
+        note += 12;
+        result *= 0.5f;
+    }
+
+    while (note > A880_NOTE)
+    {
+        note -= 12;
+        result *= 2.0f;
+    }
+
+    result *= g_noteSemitoneRatio[note - A440_NOTE];
+
+    return result;
+}
+#else
+#error Unknown temperament!
+#endif // #if EQUAL_TEMPERAMENT
